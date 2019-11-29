a44ea5f95c76400a39614d1e68bc86830a37f34e
Alecia Vogel <hello@alecia.ca>
2019-11-28 19:52:48 -0700

    Add a player controller

---
+++ lib/quackbox_web/controllers/player_controller.ex
@@ -0,0 +1,13 @@
+defmodule QuackboxWeb.PlayerController do
+  use QuackboxWeb, :controller
+  alias Quackbox.Games
+  alias Quackbox.Games.Room
+
+  def create(conn, _params) do
+    # TO DO
+  end
+
+  def show(conn, _params) do
+    # TO DO
+  end
+end
