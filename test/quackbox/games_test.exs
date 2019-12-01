41e7e62a1168569a4f058dde070df165e6f2b880
Alecia Vogel <hello@alecia.ca>
2019-11-30 22:43:57 -0700

    Create Games context and Games model

---
+++ test/quackbox/games_test.exs
@@ -0,0 +1,66 @@
+defmodule Quackbox.GamesTest do
+  use Quackbox.DataCase
+
+  alias Quackbox.Games
+
+  describe "games" do
+    alias Quackbox.Games.Game
+
+    @valid_attrs %{description: "some description", name: "some name"}
+    @update_attrs %{description: "some updated description", name: "some updated name"}
+    @invalid_attrs %{description: nil, name: nil}
+
+    def game_fixture(attrs \\ %{}) do
+      {:ok, game} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Games.create_game()
+
+      game
+    end
+
+    test "list_games/0 returns all games" do
+      game = game_fixture()
+      assert Games.list_games() == [game]
+    end
+
+    test "get_game!/1 returns the game with given id" do
+      game = game_fixture()
+      assert Games.get_game!(game.id) == game
+    end
+
+    test "create_game/1 with valid data creates a game" do
+      assert {:ok, %Game{} = game} = Games.create_game(@valid_attrs)
+      assert game.description == "some description"
+      assert game.name == "some name"
+    end
+
+    test "create_game/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Games.create_game(@invalid_attrs)
+    end
+
+    test "update_game/2 with valid data updates the game" do
+      game = game_fixture()
+      assert {:ok, %Game{} = game} = Games.update_game(game, @update_attrs)
+      assert game.description == "some updated description"
+      assert game.name == "some updated name"
+    end
+
+    test "update_game/2 with invalid data returns error changeset" do
+      game = game_fixture()
+      assert {:error, %Ecto.Changeset{}} = Games.update_game(game, @invalid_attrs)
+      assert game == Games.get_game!(game.id)
+    end
+
+    test "delete_game/1 deletes the game" do
+      game = game_fixture()
+      assert {:ok, %Game{}} = Games.delete_game(game)
+      assert_raise Ecto.NoResultsError, fn -> Games.get_game!(game.id) end
+    end
+
+    test "change_game/1 returns a game changeset" do
+      game = game_fixture()
+      assert %Ecto.Changeset{} = Games.change_game(game)
+    end
+  end
+end
