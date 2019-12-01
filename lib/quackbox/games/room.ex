c09c1c477d220d345a39fe43aa4f583cea2f7d70
Alecia Vogel <hello@alecia.ca>
2019-11-30 23:03:47 -0700

    Modify the host form and the room model

--- lib/quackbox/games/room.ex
+++ lib/quackbox/games/room.ex
@@ -1,11 +1,16 @@
 defmodule Quackbox.Games.Room do
   use Ecto.Schema
+  import Ecto.Query
   import Ecto.Changeset
 
+  alias Quackbox.Repo
+  alias Quackbox.Games.{Room}
+
   schema "rooms" do
-    field :finished_at, :date
-    field :max_players, :integer
     field :access_code, :string
+    field :max_players, :integer
+    field :finished_at, :date
+
     field :game_id, :id
     field :user_id, :id
 
@@ -15,7 +20,37 @@ defmodule Quackbox.Games.Room do
   @doc false
   def changeset(room, attrs) do
     room
-    |> cast(attrs, [:access_code, :max_players, :finished_at])
-    |> validate_required([:access_code, :max_players, :finished_at])
+    |> cast(attrs, [:game_id, :user_id, :max_players])
+    |> validate_required([:game_id, :user_id, :max_players])
+    |> generate_access_code()
+  end
+
+  @doc false
+  defp generate_access_code(%Ecto.Changeset{} = changes) do
+    case attempt_access_code() do
+      {:halt, [_code, tries]} ->
+        add_error(changes, :access_code, "After #{tries} attempts, access_code could not be generated.")
+      {:cont, tries} ->
+        add_error(changes, :access_code, "After #{tries} attempts, access_code could not be generated.")
+      [code, _tries] ->
+        put_change(changes, :access_code, code)
+      end
+  end
+
+  @doc false
+  defp attempt_access_code() do
+    Enum.reduce_while(1..5, 0, fn x, acc ->
+      access_code = Nanoid.generate()
+      
+      query = from r in Room, 
+            where: is_nil(r.finished_at),
+            where: r.access_code == ^access_code
+
+      if Repo.exists?(query) do
+        {:cont, acc + x}
+      else
+        {:halt, [access_code, acc]}
+      end
+    end)
   end
 end
