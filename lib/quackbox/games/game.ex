cf14b0befcf17b43ea30ed4d54272708f008d365
Alecia Vogel <hello@alecia.ca>
2019-12-01 18:11:41 -0700

    Update model relationships for game and room

--- lib/quackbox/games/game.ex
+++ lib/quackbox/games/game.ex
@@ -1,10 +1,13 @@
 defmodule Quackbox.Games.Game do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Quackbox.Games.Room
 
   schema "games" do
     field :description, :string
     field :name, :string
+
+    has_many :rooms, Room
 
     timestamps()
   end
