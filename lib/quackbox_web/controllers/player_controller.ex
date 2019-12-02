82b543290dd3603c07c5a60663d01a076c89f381
Alecia Vogel <hello@alecia.ca>
2019-12-01 18:08:44 -0700

    Add AudienceMember to the player_controller so it redirects properly

--- lib/quackbox_web/controllers/player_controller.ex
+++ lib/quackbox_web/controllers/player_controller.ex
@@ -1,15 +1,19 @@
 defmodule QuackboxWeb.PlayerController do
   use QuackboxWeb, :controller
   alias Quackbox.Games
-  alias Quackbox.Games.Player
+  alias Quackbox.Games.{Player, AudienceMember}
 
   def create(conn, %{"player" => %{"name" => name, "access_code" => access_code}}) do
     attrs = %{name: name, access_code: String.upcase(access_code)}
 
-    case Games.create_player(attrs) do
+    case Games.create_player_or_audience_member(attrs) do
       {:ok, %Player{} = player} ->
         conn
         |> redirect(to: Routes.room_play_path(conn, :show, access_code, player.token))
+      
+      {:ok, %AudienceMember{} = audience_member} ->
+        conn
+        |> redirect(to: Routes.room_watch_path(conn, :show, access_code, audience_member.token))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
