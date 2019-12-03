7b1526759aa595504d92651cbeffbdbed8042fc2
Alecia Vogel <hello@alecia.ca>
2019-12-02 18:23:24 -0700

    Uncomment socket.js from all 3 jsx files

--- assets/js/player.jsx
+++ assets/js/player.jsx
@@ -9,7 +9,7 @@ import 'phoenix_html'
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket"
 
 import React from 'react'
 import ReactDOM from 'react-dom'
