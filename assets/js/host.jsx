347a1426a1e425c6878705e64d713549b79105ba
Alecia Vogel <hello@alecia.ca>
2019-12-02 22:02:16 -0700

    Start messing around with react components

--- assets/js/host.jsx
+++ assets/js/host.jsx
@@ -6,11 +6,6 @@
 //
 import 'phoenix_html'
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-import socket from "./socket"
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
