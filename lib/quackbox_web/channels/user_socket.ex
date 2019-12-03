03772b6e016df54b459fed9d3b0f516acf71c7d4
Alecia Vogel <hello@alecia.ca>
2019-12-02 19:07:15 -0700

    Uncomment the room channel

--- lib/quackbox_web/channels/user_socket.ex
+++ lib/quackbox_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule QuackboxWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", QuackboxWeb.RoomChannel
+  channel "room:*", QuackboxWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
