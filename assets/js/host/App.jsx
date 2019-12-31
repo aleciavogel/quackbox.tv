acb9a28e94a01f455d18e9956f894e591b352ab4
Alecia Vogel <hello@alecia.ca>
2019-12-31 16:34:43 -0700

    Update HostApp to show currently connected users

--- assets/js/host/App.jsx
+++ assets/js/host/App.jsx
@@ -1,21 +1,49 @@
 import React, { Component } from 'react'
-import { Socket } from 'phoenix'
+import { Socket, Presence } from 'phoenix'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      players: []
+      presences: {},
+      players: [],
+      audience_members: []
     }
   }
 
   setupChannelMessageHandlers(channel) {
-    channel.on("player:joined", ({name}) => {
-      console.log(`${name} has joined the game`)
+    const self = this
+
+    channel.on('presence_state', response => {
+      self.syncPresentUsers(response)
     })
-    channel.on("audience:joined", ({name}) => {
-      console.log(`${name} has joined the game as an audience member`)
+
+    channel.on('presence_diff', response => {
+      self.syncPresentUsers(response)
+    })
+  }
+
+  syncPresentUsers(response) {
+    const players = []
+    const audience_members = []
+    const { presences: old_presences } = this.state
+    const new_presences = Presence.syncDiff(old_presences, response)
+
+    Presence.list(new_presences).map(p => {
+      const participant = p.metas[0]
+
+      if (participant.type == 'player') {
+        players.push(participant)
+      } else {
+        audience_members.push(participant)
+      }
+    })
+
+    this.setState({
+      presences: new_presences,
+      players,
+      audience_members
     })
   }
 
@@ -34,7 +62,7 @@ export default class App extends Component {
 
     let room_id = window.roomID
     let channel = socket.channel(`room:${room_id}`, {})
-    const self = this;
+    const self = this
 
     channel.join()
       .receive('ok', response => {
@@ -47,8 +75,26 @@ export default class App extends Component {
   }
 
   render() {
+    const { players, audience_members } = this.state
+
     return (
-      <h1>Welcome to the host component</h1>
+      <>
+        <h1>Welcome to the host component</h1>
+
+        Players:
+        <ul>
+          {players.map(player => (
+            <li key={player.id}>{player.name}</li>
+          ))}
+        </ul>
+
+        Audience Members:
+        <ul>
+          {audience_members.map(audience => (
+            <li key={audience.id}>{audience.name}</li>
+          ))}
+        </ul>
+      </>
     )
   }
 }
