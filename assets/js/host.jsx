5d1421643c45c64fa7276f3f515db73fa6ebf6a5
Alecia Vogel <hello@alecia.ca>
2019-12-31 18:00:26 -0700

    Update the host module to add the redux provider

--- assets/js/host.jsx
+++ assets/js/host.jsx
@@ -6,9 +6,32 @@
 //
 import 'phoenix_html'
 
+import { Socket } from 'phoenix'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import configureStore from './host/store'
 import HostApp from './host/App'
 
+const store = configureStore()
+const token = window.hostToken
+const room_id = window.roomID
+
+let socket = new Socket(
+  "/socket", 
+  {
+    params: {
+      host_token: token,
+      access_code: room_id
+    }
+  }
+)
+
 // This code starts up the React app when it runs in a browser injects the app into a DOM element.
-ReactDOM.render(<HostApp />, document.getElementById('app'))
+ReactDOM.render(
+  <Provider store={store}>
+    <HostApp socket={socket} room_id={room_id} />
+  </Provider>,
+  document.getElementById('app')
+)
