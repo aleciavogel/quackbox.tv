5edac2697c28a916f26bdec9cd759ed211a9fbc3
Alecia Vogel <hello@alecia.ca>
2020-01-11 16:31:51 -0700

    Refactor the join function into four new functions in the room channel

--- lib/quackbox_web/channels/room_channel.ex
+++ lib/quackbox_web/channels/room_channel.ex
@@ -4,48 +4,50 @@ defmodule QuackboxWeb.RoomChannel do
   alias Quackbox.Games.{Player, AudienceMember}
   alias QuackboxWeb.Presence
 
-  def join("room:" <> access_code, _params, socket) do
-    player_id = socket.assigns[:current_player_id]
-    audience_id = socket.assigns[:current_audience_id]
-    host_id = socket.assigns[:current_host_id]
+  # Player joins the game
+  def join("room:" <> access_code, _params, %{assigns: %{current_player_id: player_id}} = socket) do
+    player = 
+      Repo.get(Player, player_id)
+      |> Repo.preload(:room)
 
-    cond do
-      player_id != nil ->
-        player = 
-          Repo.get(Player, player_id)
-          |> Repo.preload(:room)
-        if player.room.access_code === access_code do
-          player_info = %{
-            name: player.name,
-            id: player.id
-          }
-          send(self(), {:after_player_join, player_info})
-          {:ok, %{channel: "room:#{access_code}"}, socket}
-        else
-          {:error, %{reason: "Invalid session."}}
-        end
-
-      audience_id != nil ->
-        audience = 
-          Repo.get(AudienceMember, audience_id)
-          |> Repo.preload(:room)
-        if audience.room.access_code === access_code do
-          audience_info = %{
-            name: audience.name,
-            id: audience.id
-          }
-          send(self(), {:after_player_join, audience_info})
-          {:ok, %{channel: "room:#{access_code}"}, socket}
-        else
-          {:error, %{reason: "Invalid session."}}
-        end
-      
-      host_id != nil ->
-        {:ok, %{channel: "room:#{access_code}", presences: Presence.list(socket)}, socket}
-
-      true ->
-        {:error, %{reason: "Invalid session."}}
+    if player.room.access_code === access_code do
+      player_info = %{
+        name: player.name,
+        id: player.id
+      }
+      send(self(), {:after_player_join, player_info})
+      {:ok, %{channel: "room:#{access_code}"}, socket}
+    else
+      {:error, %{reason: "Invalid session."}}
     end
+  end
+  
+  # Audience joins the game
+  def join("room:" <> access_code, _params, %{assigns: %{current_audience_id: audience_id}} = socket) do
+    audience = 
+      Repo.get(AudienceMember, audience_id)
+      |> Repo.preload(:room)
+
+    if audience.room.access_code === access_code do
+      audience_info = %{
+        name: audience.name,
+        id: audience.id
+      }
+      send(self(), {:after_player_join, audience_info})
+      {:ok, %{channel: "room:#{access_code}"}, socket}
+    else
+      {:error, %{reason: "Invalid session."}}
+    end
+  end
+  
+  # Host joins the game
+  def join("room:" <> access_code, _params, %{assigns: %{current_host_id: host_id}} = socket) do
+    {:ok, %{channel: "room:#{access_code}", presences: Presence.list(socket)}, socket}
+  end
+  
+  # Refuse all other attempts to join
+  def join(_room, _params, _socket) do
+    {:error, %{reason: "Invalid session."}}
   end
 
   def handle_info({:after_player_join, player}, socket) do
