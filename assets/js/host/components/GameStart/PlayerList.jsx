e6c315c1cd755fae618d74429c8ce4d8a94b5fcf
Alecia Vogel <hello@alecia.ca>
2020-01-12 01:48:55 -0700

    Get rid of the lead_player_id in PlayerListItem, prettify

--- assets/js/host/components/GameStart/PlayerList.jsx
+++ assets/js/host/components/GameStart/PlayerList.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { makeStyles } from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
-import PlayerListItem from './PlayerListItem'
+import PlayerListItem from "./PlayerListItem";
 
 const useStyles = makeStyles(theme => ({
   root: {
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2)
   }
-}))
+}));
 
-const PlayerList = ({ players, lead_player_id }) => {
-  const classes = useStyles()
-  const player_items = players.map((player) => (
+const PlayerList = ({ players }) => {
+  const classes = useStyles();
+  const player_items = players.map(player => (
     <PlayerListItem player={player} key={player.id} />
-  ))
+  ));
 
   return (
     <Box className={classes.root}>
@@ -27,15 +27,14 @@ const PlayerList = ({ players, lead_player_id }) => {
 
       {player_items}
     </Box>
-  )
-}
+  );
+};
 
 function mapStateToProps(state) {
-  const { players, lead_player_id } = state
+  const { players } = state;
   return {
-    players,
-    lead_player_id
-  }
+    players
+  };
 }
 
-export default connect(mapStateToProps)(PlayerList)
+export default connect(mapStateToProps)(PlayerList);
